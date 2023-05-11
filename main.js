adf9f9f2983d6a22a5a536afb0ec8d4d175cdd90
Oscar León <oscarleon.alcatel@gmail.com>
2023-05-11 14:23:07 -0600

    co# Please enter the commit message for your changes. Lines starting

---
+++ main.js
@@ -0,0 +1,11 @@
+let nombre = "Oscar"
+let apellido = "León"
+let userplatzi = "oscarleon.app"
+let edad = 33
+let email = "info@oscarleon.app"
+let mayorEdad = true;
+let dineroAhorrado = 37000;
+let deudas = 0;
+
+console.log(nombre + apellido)
+console.log (dineroAhorrado - deudas)
